dc8f4075c2a60f4bc5786d6411c2405305b86b78
Serena Lew <36708701+minibaguettes@users.noreply.github.com>
2022-06-28 14:59:58 -0700

    s90 create action, validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,26 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # require top lvl key or article and permit title and description to be used to create article object
+    # render plain: @article.inspect
+    # render plain: @article will display the article object
+    # .inspect will display the article information; id, title, description
+    if @article.save
+      flash[:notice] = "Article was created successfully."  # ruby helper "flash", parameter = name
+      redirect_to @article
+    else
+      render 'new'
+    end
+    #redirect_to article_path(@article)
+    # extracts id from @article and uses to form path
+
+  end
+
 end
