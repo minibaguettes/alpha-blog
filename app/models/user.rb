e1d912ccfede34e95d8db4731a66dd0cc9c49483
minibaguettes <serenalew1124@gmail.com>
2022-07-14 14:46:24 -0700

    added signup form (validations for edit dont work)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase } # self references each object of user class
   has_many :articles  # user can have many articles
   # ensures username/email is present before user object is saved to database
   # uniqueness: true => checks for case sensitivity by default, but don't want that, so specify case
@@ -6,4 +7,5 @@ class User < ApplicationRecord
   # check for valid email by using regex as a constant 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with:VALID_EMAIL_REGEX } 
+  has_secure_password 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_12_233408) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_13_005149) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_12_233408) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
