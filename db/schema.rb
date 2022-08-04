d0f499753270dd47959034a6aea12361db3d3af3
minibaguettes <serenalew1124@gmail.com>
2022-08-04 16:05:51 -0700

    restrict actions for users (logged in/out)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_13_005149) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_04_220156) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "description"
